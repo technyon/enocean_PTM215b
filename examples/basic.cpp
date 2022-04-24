662bd3342f9824f50ae8650e5fd6f5933c8808c7
technyon <j.o.schuemann@gmx.de>
2022-04-24 10:27:58 +0200

    fix basic example

--- examples/basic.cpp
+++ examples/basic.cpp
@@ -1,14 +1,12 @@
 /**
  * A BLE client example to receive events from an Enocean PTM215b switch
- * 
- * 
+ *
+ *
  * author Jeroen
  */
 
 #include "enocean_PTM215b.h"
 
-#define STRINGIFY_(x) #x
-#define STRING(VAR) STRINGIFY_(VAR)
 #define BLE_ADDRESS "e2:15:00:01:72:f9"
 #define SECURITY_KEY "1CB95CE63F19ADC7E0FB92DA56D69219"
 
@@ -19,49 +17,43 @@ public:
 
     void handleEvent(PTM215b::SwitchEvent& evt) override {
 
-      std::string type;
-      switch (evt.eventType) {
-      case PTM215b::EventType::Pushed:
-        type = "Pushed";
-        break;
-      case PTM215b::EventType::Repeat:
-        type = "Repeat";
-        break;
-      case PTM215b::EventType::ReleaseLong:
-        type = "ReleasedLong";
-        break;
-      
-      case PTM215b::EventType::ReleaseShort:
-        type = "ReleasedShort";
-        break;
+        std::string type;
+        switch (evt.eventType) {
+            case PTM215b::EventType::Pushed:
+                type = "Pushed";
+                break;
+            case PTM215b::EventType::Repeat:
+                type = "Repeat";
+                break;
+            case PTM215b::EventType::ReleaseLong:
+                type = "ReleasedLong";
+                break;
+            case PTM215b::EventType::ReleaseShort:
+                type = "ReleasedShort";
+                break;
+            default:
+                break;
+        }
 
-      default:
-        break;
-      }
+        std::string direction = (evt.button == PTM215b::Button::A_UP || evt.button == PTM215b::Button::B_UP) ? "Up" : "Down";
 
-      std::string direction = (evt.direction == PTM215b::Direction::Up) ? "Up" : "Down";
-
-      log_d("BleSwitchEvent Received: Node Id: %d, Type: %s, Direction: %s", evt.nodeId, type.c_str(), direction.c_str());
+        log_d("BleSwitchEvent Received: Node Id: %d, Type: %s, Direction: %s", evt.nodeId, type.c_str(), direction.c_str());
     };
 
 };
 
 EH handler;
-PTM215b::Enocean_PTM215b enocean_PTM215b(true);
+PTM215b::Enocean_PTM215b enocean_PTM215b(handler, true);
 
 void setup(){
-  Serial.begin(115200);
-  log_d("Starting Enocean_PTM215b BLE Example application...");
-    
-  BLEDevice::init("ESP32_client");
-  enocean_PTM215b.initialize();
+    Serial.begin(115200);
 
-  enocean_PTM215b.registerBleSwitch(STRING(BLE_ADDRESS), STRING(SECURITY_KEY), 0, 1, &handler);
+    BLEDevice::init("ESP32_client");
+    enocean_PTM215b.initialize();
 
+    enocean_PTM215b.registerBleSwitch(BLE_ADDRESS, SECURITY_KEY, 0, 1, 2 , 3);
 }
 
 void loop(){
-
     delay(1000);
-
-} 
+}
